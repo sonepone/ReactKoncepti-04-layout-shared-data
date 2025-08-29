54962a6888338bbc506f20a23ed8a29972ff0160
Nebojsa Stefanovic <cordunas@gmail.com>
2025-08-29 16:03:04 +0200

    Update PostsLayout.js

--- src/pages/PostsLayout.js
+++ src/pages/PostsLayout.js
@@ -25,7 +25,7 @@ export default PostsLayout;
 
 export async function loader() {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  throw new Error('Proba bacanja greske. Da li originalna komponenta moze biti errorElement.');
+  
   if (!response.ok) {
     throw new Error('Could not fetch posts');
   }
