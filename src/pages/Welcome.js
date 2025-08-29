067a7fcfcec9be0a4ea0624e9cbfd066c5b658ae
Nebojsa Stefanovic <cordunas@gmail.com>
2025-08-29 08:50:27 +0200

    Izmjena u komponenti Welcome da znam cim otvorim site na sta se odnosi.

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,6 +3,7 @@ function Welcome() {
     <main>
       <h1>Welcome</h1>
       <p>Learn all about React Router and its data fetching capabilities!</p>
+      <p>Ovo je primjer iz Maxove knjige iz poglavlja 13, potpoglavlje: "Layouts Revisited"</p>
     </main>
   );
 }
